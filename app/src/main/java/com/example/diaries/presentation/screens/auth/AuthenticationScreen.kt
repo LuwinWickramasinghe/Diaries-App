4697f67d68a80283bf7a5eab57ff2ca0052fd49f
iamluwin <luwin.anuka@gmail.com>
2024-08-29 02:59:55 +0530

    UI design changes

--- app/src/main/java/com/example/diaries/presentation/screens/auth/AuthenticationScreen.kt
+++ app/src/main/java/com/example/diaries/presentation/screens/auth/AuthenticationScreen.kt
@@ -2,8 +2,14 @@ package com.example.diaries.presentation.screens.auth
 
 import android.annotation.SuppressLint
 import android.util.Log
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
 import com.example.diaries.util.Constants.CLIENT_ID
 import com.stevdzasan.messagebar.ContentWithMessageBar
 import com.stevdzasan.messagebar.MessageBarState
@@ -13,15 +19,21 @@ import com.stevdzasan.onetap.OneTapSignInWithGoogle
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun AuthenticationScreen(
+    authenticated: Boolean,
     loadingState: Boolean,
     oneTapState: OneTapSignInState,
     messageBarState : MessageBarState,
     onButtonClicked: () -> Unit,
     onTokenIdReceived: (String) -> Unit,
     onDialogDismissed:(String) -> Unit,
+    navigateToHome:() -> Unit
 
 ){
     Scaffold (
+        modifier = Modifier.
+        background(MaterialTheme.colorScheme.surface).
+        navigationBarsPadding().
+        statusBarsPadding(),
         content = {
             ContentWithMessageBar(messageBarState = messageBarState) {
                 AuthenticationContent(
@@ -44,4 +56,11 @@ fun AuthenticationScreen(
 
 
         })
+
+    LaunchedEffect(key1 = authenticated) {
+        if(authenticated){
+            navigateToHome()
+
+        }
+    }
 }
