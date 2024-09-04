25e1005f38f61641a839da081d18bc4d55b42719
iamluwin <luwin.anuka@gmail.com>
2024-09-04 17:27:00 +0530

    navigation drawer

--- app/src/main/java/com/example/diaries/presentation/screens/home/HomeScreen.kt
+++ app/src/main/java/com/example/diaries/presentation/screens/home/HomeScreen.kt
@@ -1,34 +1,106 @@
 package com.example.diaries.presentation.screens.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Edit
+import androidx.compose.material3.DrawerState
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.ModalDrawerSheet
+import androidx.compose.material3.ModalNavigationDrawer
+import androidx.compose.material3.NavigationDrawerItem
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.example.diaries.R
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun HomeScreen(
+    drawerState: DrawerState,
     onMenuClicked: () -> Unit,
+    onSignOutClick: () -> Unit,
     navigateToWrite: () -> Unit
 ) {
-    Scaffold (
-        topBar = {
-            HomeTopBar (onMenuClicked = onMenuClicked)
-        },
-        floatingActionButton = {
-            FloatingActionButton(onClick = navigateToWrite){
-                Icon(
-                    imageVector = Icons.Default.Edit,
-                    contentDescription = "New diary"
-                )
+    NavigationDrawer(
+        drawerState = drawerState,
+        onSignOutClick = onSignOutClick
+    ) {
+        Scaffold (
+            topBar = {
+                HomeTopBar (onMenuClicked = onMenuClicked)
+            },
+            floatingActionButton = {
+                FloatingActionButton(onClick = navigateToWrite){
+                    Icon(
+                        imageVector = Icons.Default.Edit,
+                        contentDescription = "New diary"
+                    )
+                }
+            },
+            content = {
+
             }
+
+        )
+    }
+}
+
+@Composable
+fun NavigationDrawer(
+    drawerState: DrawerState,
+    onSignOutClick : () ->Unit,
+    content: @Composable () -> Unit
+) {
+    ModalNavigationDrawer(
+        drawerState = drawerState,
+        drawerContent = {
+            ModalDrawerSheet(
+                content = {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .height(250.dp),
+                        contentAlignment = Alignment.Center
+                    )
+                    {
+                        Image(
+                            modifier = Modifier.size(250.dp),
+                            painter = painterResource(id = R.drawable.logo),
+                            contentDescription = "Logo Image"
+                        )
+                    }
+                    NavigationDrawerItem(
+                        label = {
+                            Row(modifier = Modifier.padding(horizontal = 12.dp)) {
+                                Image(
+                                    painter = painterResource(id = R.drawable.google_logo),
+                                    contentDescription = "Google logo for sign out"
+                                )
+                                Spacer(modifier = Modifier.width(12.dp))
+                                Text(text = "Sign Out", color = MaterialTheme.colorScheme.onSurface)
+                            }
+                        },
+                        selected = false,
+                        onClick = onSignOutClick
+                    )
+                }
+            )
         },
-        content = {
-
-        }
-
+        content = content
     )
 }
