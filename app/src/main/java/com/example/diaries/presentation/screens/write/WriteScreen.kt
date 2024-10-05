1d975e33c9e60821be1ba40be006f3fc9a28e6d7
iamluwin <luwin.anuka@gmail.com>
2024-10-05 23:52:47 +0530

    write screen scaffold

---
+++ app/src/main/java/com/example/diaries/presentation/screens/write/WriteScreen.kt
@@ -0,0 +1,18 @@
+package com.example.diaries.presentation.screens.write
+
+import android.annotation.SuppressLint
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Composable
+fun WriteScreen() {
+    Scaffold(
+        topBar = {
+
+        },
+        content = {
+
+        }
+    )
+}
