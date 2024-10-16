6b233d43fd7e275a1a32c62b1d7f9e8beb71d6b9
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-10-17 01:02:45 +0530

    main write view component added

--- app/src/main/java/com/example/diaries/presentation/screens/write/WriteScreen.kt
+++ app/src/main/java/com/example/diaries/presentation/screens/write/WriteScreen.kt
@@ -1,13 +1,17 @@
 package com.example.diaries.presentation.screens.write
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.pager.PagerState
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import com.example.diaries.model.Diary
 
+@OptIn(ExperimentalFoundationApi::class)
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun WriteScreen(
+    pagerState: PagerState,
     selectedDiary: Diary?,
     onBackClicked: () -> Unit,
     onDeleteYes: () -> Unit
