9db427ab105db065c5b3583825ad52e1e6b10b62
Luwin Wickramasinghe <luwin.anuka@gmail.com>
2024-10-16 23:46:01 +0530

    added calender and drop down menu for delete diaries

--- app/src/main/java/com/example/diaries/presentation/screens/write/WriteScreen.kt
+++ app/src/main/java/com/example/diaries/presentation/screens/write/WriteScreen.kt
@@ -3,13 +3,22 @@ package com.example.diaries.presentation.screens.write
 import android.annotation.SuppressLint
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import com.example.diaries.model.Diary
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
-fun WriteScreen(onBackClicked: () -> Unit) {
+fun WriteScreen(
+    selectedDiary: Diary?,
+    onBackClicked: () -> Unit,
+    onDeleteYes: () -> Unit
+) {
     Scaffold(
         topBar = {
-            WriteTopBar(onBackClicked = onBackClicked)
+            WriteTopBar(
+                selectedDiary = selectedDiary,
+                onDeleteYes = onDeleteYes,
+                onBackClicked = onBackClicked
+            )
         },
         content = {
 
--- app/src/main/java/com/example/diaries/presentation/screens/write/WriteTopBar.kt
+++ app/src/main/java/com/example/diaries/presentation/screens/write/WriteTopBar.kt
@@ -4,22 +4,39 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
+import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import com.example.diaries.model.Diary
+import com.example.diaries.presentation.components.DisplayAlertDialog
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun WriteTopBar(
-    onBackClicked: () -> Unit
+    selectedDiary: Diary?,
+    onBackClicked: () -> Unit,
+    onDeleteYes: () -> Unit
 ) {
     CenterAlignedTopAppBar(
         title = {
@@ -52,7 +69,62 @@ fun WriteTopBar(
                     contentDescription = "Back button"
                 )
             }
+        },
+        actions = {
+            IconButton(onClick = {}) {
+                Icon(
+                    imageVector = Icons.Default.DateRange,
+                    contentDescription = "Date icon"
+                )
+            }
+            if (selectedDiary != null) {
+                DeleteDiary(
+                    selectedDiary,
+                    onDeleteYes = onDeleteYes
+                )
+            }
         }
     )
+
+}
+
+@Composable
+fun DeleteDiary(
+    selectedDiary: Diary?,
+    onDeleteYes: () -> Unit
+){
+    var expanded by remember { mutableStateOf(false) }
+    var openDialog by remember { mutableStateOf(false) }
+    DropdownMenu(
+        expanded = expanded,
+        onDismissRequest = {expanded = false}
+    ) {
+        DropdownMenuItem(
+            text = {
+                Text(text = "Delete")
+            }, onClick = {
+                openDialog = true
+                expanded = true
+            }
+        )
+    }
+    DisplayAlertDialog(
+        title = "Delete",
+        message = "Are you sure you want to delete this diary permanantly?'$(selectedDiary?.title)' ",
+        dialogOpened = openDialog,
+        onDialogClosed = {openDialog = false},
+        onYesClicked = onDeleteYes
+    )
+    IconButton(
+        onClick = {expanded = !expanded}
+    )
+
+         {
+            Icon(
+                imageVector = Icons.Default.MoreVert,
+                contentDescription = "Oberflow menu icon",
+                tint = MaterialTheme.colorScheme.onSurface
+            )
+        }
 
 }
